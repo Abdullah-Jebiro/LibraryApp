13169d4fdf85613fff50f30b7145a4cf76c7a111
Abdullah Jbero <Abdullahjbero@gmail.com>
2022-09-19 23:53:59 +0300

    Add  Services

--- LibraryMain/Program.cs
+++ LibraryMain/Program.cs
@@ -1,72 +1,49 @@
 ﻿using LibraryData;
 using LibraryDemian;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Services;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
+using System.Threading.Channels;
+using static Services.Services;
+
 
 namespace LibraryMain
 {
     internal class Program
     {
-        private static void AdddefualtUsers()
-        {
-            using (var context = new DB())
-            {
-                if (context.Users.Any())
-                {
-                    string cotents = File.ReadAllText("default.json");
-                    var defualtUsers = JsonSerializer.Deserialize<List<User>>(cotents);
-                    context.Users.AddRange(defualtUsers);
-                    context.SaveChanges();
-
-                }
-            }
-        }
-        private static bool IsUser()
-        {
-            using (var context = new DB())
-            {
-                Console.Write("Enter UserName: ");
-                string UserName = Console.ReadLine();
-                Console.Write("Enter Password: ");
-                string Password = Console.ReadLine();
-                return context.Users.Any(u => u.UserName == UserName && u.Password == Password);
-            }
-        }
-        private static int BooksCount()
-        {
-            using (var context = new DB())
-            {
-                return context.Books.Count();
-            }
-        }
+   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            AdddefualtUsers();         
+            
+            Console.WriteLine("Hello");
+            AdddefualtUsers();
             if (IsUser())
             {
-
                 Console.WriteLine($"Number of books {BooksCount()}");
-                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"\r\n" +
+                    "1 => Inquiry about books\r\n" +
+                    "2 =>  Bring book data\r\n" +
+                    "3 =>  Delete a book\r\n" +
+                    "4 =>  Update book data\r\n" +
+                    "or any other key to exit");
+
+                string num =Console.ReadLine();
                 switch (num)
                 {
-                    case 1:
-
+                    case "1":
+                        GetInfoBooks();
                         break;
-                    case 2:
-
+                    case "2":
+                        GetBook();
                         break;
-                    case 3:
-
+                    case "3":
+                        DeleteBook();
                         break;
-                    case 4:
-
+                    case "4":
+                        UpdateBook();
                         break;
-                    default:
-                        break;
-
                 }
-
             }
         }
     }
---
+++ Services/Services.cs
@@ -0,0 +1,90 @@
+﻿using LibraryData;
+using LibraryDemian;
+using System.Text.Json;
+
+namespace Services
+{
+    public class Services
+    {
+        public static void AdddefualtUsers()
+        {
+            using (var context = new DB())
+            {
+                if (!context.Users.Any())
+                {
+                    string cotents = File.ReadAllText("default.json");
+                    var defualtUsers = JsonSerializer.Deserialize<List<User>>(cotents);
+                    context.Users.AddRange(defualtUsers);
+                    context.SaveChanges();
+
+                }
+            }
+        }
+        public static bool IsUser()
+        {
+            using (var context = new DB())
+            {
+                Console.Write("Enter UserName:");
+                string UserName = Console.ReadLine().Trim();
+                Console.Write("Enter Password:");
+                string Password = Console.ReadLine().Trim();
+                bool isUser = context.Users.Any(u => u.UserName == UserName && u.Password == Password);
+                return isUser;
+            }
+        }
+        public static int BooksCount()
+        {
+            using (var context = new DB())
+            {
+                return context.Books.Count();
+            }
+        }
+        public static void GetInfoBooks()
+        {
+            using (var context = new DB())
+            {
+                var books = context.Books.Select(b => new { b.BookId, b.Title, b.Count }).ToList(); ;
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"Id:{book.BookId} Title:{book.Title} Count:{book.Count}");
+                }
+                Console.WriteLine(context.Books.Sum(b => b.Count));
+            }
+        }
+        public static void GetBook()
+        {
+            Console.Write("Enter Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            using (var context = new DB())
+            {
+                var book = context.Books.SingleOrDefault(b => b.BookId == id);
+                Console.WriteLine($"Id:{book.BookId} Title:{book.Title} Count:{book.Count} Price {book.Price}");
+            }
+        }
+        public static void DeleteBook()
+        {
+            Console.Write("Enter Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            using (var context = new DB())
+            {
+                var book = context.Books.SingleOrDefault(b => b.BookId == id);
+                context.Books.Remove(book);
+                context.SaveChanges();
+            }
+        }
+        public static void UpdateBook()
+        {
+            Console.Write("Enter Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            using (var context = new DB())
+            {
+                var book = context.Books.SingleOrDefault(b => b.BookId == id);
+                Console.Write("Enter Title:");
+                book.Title = Console.ReadLine();
+                Console.Write("Enter Price:");
+                book.Price = Convert.ToInt32(Console.ReadLine());
+                context.SaveChanges();
+            }
+        }
+    }
+}
